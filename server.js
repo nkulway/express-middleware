e01b83240f63a04fb14ef1c486d9f8b8d8a752dc
Nick Kulway <nkulway@gmail.com>
2021-10-07 20:53:02 -0400

    captured logger and helmet middleware//static public folder

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
+const logger = morgan('tiny')
+const helmet = require('helmet')
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -7,7 +10,12 @@ const port = 3000
 const app = express()
 const server = http.createServer(app)
 
-
+// log all requests to console 
+app.use(logger)
+// helmet secures against common security issues
+app.use(helmet())
+// look for files in the public folder
+app.use(express.static('public'))
 
 server.listen(port, hostname, () => {
     console.log(`Server running on http://${hostname}:${port}`)
