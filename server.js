c25b7f031f88ce03d7a83191a137df2db93da9f1
Nick Kulway <nkulway@gmail.com>
2021-10-07 20:02:39 -0400

    create empty express boiler plate

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const express = require('express')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const app = express()
+const server = http.createServer(app)
+
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running on http://${hostname}:${port}`)
+})
